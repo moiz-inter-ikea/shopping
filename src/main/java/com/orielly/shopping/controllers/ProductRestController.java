cde9f0771e2c946f721f26be84a7f41bd6bec281
Moiz <syed.moiz27@gmail.com>
2021-04-23 15:40:05 +0200

    after post

--- src/main/java/com/orielly/shopping/controllers/ProductRestController.java
+++ src/main/java/com/orielly/shopping/controllers/ProductRestController.java
@@ -4,6 +4,7 @@ import com.orielly.shopping.entities.Product;
 import com.orielly.shopping.exception.ProductNotFoundException;
 import com.orielly.shopping.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -43,6 +44,10 @@ public class ProductRestController {
                 .toUri();
 
         return ResponseEntity.created(location).body(p);
+
+        //Way to just return 201 & Created
+        //public ResponseEntity<Product> addProduct(@RequestBody Product product){
+        //return new ResponseEntity<> ("CREATED",HttpStatus.CREATED); }
 
     }
 }
